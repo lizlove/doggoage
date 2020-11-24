63d92645207f888ced023546983e6c3928e04850
lizlove <elizabethlovero@gmail.com>
2020-11-24 13:20:26 -0500

    Form template.

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { App } from './App';
+import { App } from '../components/App';
 
-ReactDOM.render(<App dogage={12} unit={'months'} humanage={null} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
